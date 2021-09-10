96efefb3a7c3f722b28bab964fc4f35cc5e4e4f5
neftec99 <neftalitorres@tec.mx>
2021-09-10 11:44:42 -0500

    video : 297

--- src/app/pages/pagina1/pagina1.component.ts
+++ src/app/pages/pagina1/pagina1.component.ts
@@ -1,4 +1,5 @@
 import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-pagina1',
@@ -11,6 +12,8 @@ export class Pagina1Component
             AfterViewInit,AfterViewChecked,OnDestroy {
 
   nombre: string="Israel";
+  segundos : number =0;
+  timerSubscription! : Subscription;
 
   constructor() { }
   ngOnChanges(changes: SimpleChanges): void {
@@ -33,11 +36,16 @@ export class Pagina1Component
   }
   ngOnDestroy(): void {
     console.log('ngOnDestroy');
+    this.timerSubscription.unsubscribe();
+    console.log("Timer limpiado");
   }
 
   
   ngOnInit(): void {
     console.log('ngOnInit');
+    this.timerSubscription = interval(1000).subscribe(i =>{
+      this.segundos= i;
+    })
   }
 
 
